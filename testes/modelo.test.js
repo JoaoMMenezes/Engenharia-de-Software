e80b86f859b87ab42c29acabc58196fb650b12fb
JoaoMMenezes <joaopm.m0@gmail.com>
2023-05-29 16:06:42 -0300

    implementações de  modelo.test

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,24 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
-  const perguntas = modelo.listar_perguntas(); 
+  const perguntas = modelo.listar_perguntas();
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  const pergunta = modelo.get_pergunta(perguntas[0].id_pergunta)
+  expect(pergunta.texto).toBe('1 + 1 = ?')
 });
+
+test('Testando cadastro de respostas', ()=> {
+  modelo.cadastrar_pergunta('10 + 10 = ?');
+  const perguntas = modelo.listar_perguntas();
+  expect(perguntas.length).toBe(1);
+  modelo.cadastrar_resposta(perguntas[0].id_pergunta, "20")
+  modelo.cadastrar_resposta(perguntas[0].id_pergunta, "1")
+  const respostas = modelo.get_respostas(perguntas[0].id_pergunta)
+  expect(respostas[0].texto).toBe("20")
+  expect(respostas[1].texto).toBe("1")
+  expect(modelo.get_num_respostas(perguntas[0].id_pergunta)).toBe(2)
+})
